a4b2d3070c1f544387dd7f37215ed89f8e3b6df8
ibhesholihin <ibhe07@icloud.com>
2023-05-09 19:34:55 +0800

    order process revision

--- apps/http/handler/order.go
+++ apps/http/handler/order.go
@@ -1,14 +1,28 @@
 package handler
 
 import (
+	"net/http"
+
+	"github.com/ibhesholihin/hevent/apps/models"
 	"github.com/ibhesholihin/hevent/apps/service"
+	"github.com/ibhesholihin/hevent/utils"
 	"github.com/labstack/echo/v4"
 )
 
 type (
 	OrderHandler interface {
-		GetUserOrder(c echo.Context) error
-		AddUserOrder(c echo.Context) error
+		GetCartSession(c echo.Context) error
+		AddItemToCart(c echo.Context) error
+		GetItemsCart(c echo.Context) error
+
+		DeleteItemFromCart(c echo.Context) error
+		CreateOrder(c echo.Context) error
+		GetListOrders(c echo.Context) error
+		GetOrderById(c echo.Context) error
+		UploadReceipt(c echo.Context) error
+
+		//test payment handler
+		TestPayment(c echo.Context) error
 	}
 
 	orderHandler struct {
@@ -16,10 +30,75 @@ type (
 	}
 )
 
-func (h *orderHandler) GetUserOrder(c echo.Context) error {
+func (h *orderHandler) GetCartSession(c echo.Context) error {
+	//ctx := c.Request().Context()
+	//uid := c.Get("user_id").(int64)
+
 	return nil
 }
 
-func (h *orderHandler) AddUserOrder(c echo.Context) error {
+func (h *orderHandler) AddItemToCart(c echo.Context) error {
+	//ctx := c.Request().Context()
+	//h.PayService.GeneratePayReq(ctx,"ibhe","ibhe@admin.com","123",30000)
+
 	return nil
+}
+
+func (h *orderHandler) GetItemsCart(c echo.Context) error {
+	return nil
+}
+
+func (h *orderHandler) DeleteItemFromCart(c echo.Context) error {
+	return nil
+}
+
+func (h *orderHandler) CreateOrder(c echo.Context) error {
+	return nil
+}
+
+func (h *orderHandler) GetListOrders(c echo.Context) error {
+	return nil
+}
+
+func (h *orderHandler) GetOrderById(c echo.Context) error {
+	return nil
+}
+
+func (h *orderHandler) UploadReceipt(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	return c.JSON(http.StatusOK, models.HttpResponse{
+		Code:    http.StatusOK,
+		Message: "Success",
+		Data:    ctx,
+	})
+}
+
+func (h *orderHandler) TestPayment(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	type payment struct {
+		Tokenpay string `json:"token"`
+		Urlpay   string `json:"url"`
+	}
+
+	payID, payUrl, err := h.OrderService.TestPayment(ctx)
+
+	pay := &payment{
+		Tokenpay: payID,
+		Urlpay:   payUrl,
+	}
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, models.HttpResponse{
+			Code:    http.StatusInternalServerError,
+			Message: "Internal Server Error",
+			Data:    utils.ErrInternalServerError,
+		})
+	}
+
+	return c.JSON(http.StatusOK, models.HttpResponse{
+		Code:    http.StatusOK,
+		Message: "Order Success",
+		Data:    pay,
+	})
 }
--- apps/service/order.go
+++ apps/service/order.go
@@ -2,29 +2,35 @@ package service
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"time"
 
 	md "github.com/ibhesholihin/hevent/apps/models"
 	"github.com/ibhesholihin/hevent/apps/repository"
+	"github.com/ibhesholihin/hevent/utils/paygate"
 )
 
 type (
 
 	//init service contract
 	OrderService interface {
-		//FindListCategory(c context.Context) ([]md.EventCategory, error)
+		FindListOrder(c context.Context) ([]md.EventCategory, error)
 		//AddCategory(c context.Context, adminReq md.CreateEventCategoryReq) (md.CreateEventCategoryRes, error)
+
+		TestPayment(c context.Context) (string, string, error)
 	}
 
 	//init service parameter
 	orderService struct {
 		stores         *repository.Stores
 		contextTimeout time.Duration
+		payment        paygate.PayService
 	}
 )
 
-// Event Category Get List
-func (s *eventService) FindListOrder(c context.Context) ([]md.EventCategory, error) {
+// Get List order
+func (s *orderService) FindListOrder(c context.Context) ([]md.EventCategory, error) {
 	_, cancel := context.WithTimeout(c, s.contextTimeout)
 	defer cancel()
 
@@ -36,7 +42,7 @@ func (s *eventService) FindListOrder(c context.Context) ([]md.EventCategory, err
 }
 
 // Create User Cart Order
-func (s *eventService) CreateCart(c context.Context, catReq md.CreateEventCategoryReq) (md.CreateEventCategoryRes, error) {
+func (s *orderService) CreateCart(c context.Context, catReq md.CreateEventCategoryReq) (md.CreateEventCategoryRes, error) {
 	_, cancel := context.WithTimeout(c, s.contextTimeout)
 	defer cancel()
 
@@ -49,4 +55,19 @@ func (s *eventService) CreateCart(c context.Context, catReq md.CreateEventCatego
 		ID: kategori.ID, Category: kategori.Category,
 	}
 	return katResp, nil
+}
+
+// Test Payment
+func (s *orderService) TestPayment(c context.Context) (string, string, error) {
+	ctx, cancel := context.WithTimeout(c, s.contextTimeout)
+	defer cancel()
+
+	payID, payUrl, err := s.payment.GeneratePayReq(ctx, "ibhe", "redaksi@herald.id", "114444", 2000000)
+
+	fmt.Println("URl :", payUrl)
+
+	if err != nil {
+		return "failed", "", errors.New("error")
+	}
+	return payID, payUrl, nil
 }
